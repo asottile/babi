61ef817f8a33cff49f51131e50200b31377c9b18
Anthony Sottile <asottile@umich.edu>
2022-02-19 13:21:00 -0500

    don't crash on opening non-utf-8 files

--- tests/features/save_test.py
+++ tests/features/save_test.py
@@ -53,6 +53,14 @@ def test_not_a_file(run, tmpdir):
         h.await_text("d' is not a file")
 
 
+def test_non_utf8_file(run, tmpdir):
+    f = tmpdir.join('f')
+    f.write_binary(b'\x98\xef\xa0\x12')
+
+    with run(str(f)) as h, and_exit(h):
+        h.await_text("f' is non-utf-8")
+
+
 def test_save_no_filename_specified(run, tmpdir):
     f = tmpdir.join('f')
 
